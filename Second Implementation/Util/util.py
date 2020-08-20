04db1f61785a4bfdac73f084e228496c60dcb122
Tales Scopinho <36113553+talicopanda@users.noreply.github.com>
2020-08-20 13:41:06 -0300

    Implementation of different cost functions

--- Second Implementation/Util/util.py
+++ Second Implementation/Util/util.py
@@ -48,4 +48,78 @@ def compute_cost(Y, Y_hat):
 
     return cost, dY_hat
 
+# ------------- Different Cost Functions -----------------
 
+def compute_bce_cost(Y, P_hat):
+    """
+    This function computes Binary Cross-Entropy(bce) Cost and returns the Cost and its
+    derivative.
+    This function uses the following Binary Cross-Entropy Cost defined as:
+    => (1/m) * np.sum(-Y*np.log(P_hat) - (1-Y)*np.log(1-P_hat))
+    Args:
+        Y: labels of data
+        P_hat: Estimated output probabilities from the last layer, the output layer
+    Returns:
+        cost: The Binary Cross-Entropy Cost result
+        dP_hat: gradient of Cost w.r.t P_hat
+    """
+    m = Y.shape[1]  # m -> number of examples in the batch
+
+    cost = (1/m) * np.sum(-Y*np.log(P_hat) - (1-Y)*np.log(1-P_hat))
+    cost = np.squeeze(cost)  # remove extraneous dimensions to give just a scalar (e.g. this turns [[17]] into 17)
+
+    dP_hat = (1/m) * (-(Y/P_hat) + ((1-Y)/(1-P_hat)))
+
+    return cost, dP_hat
+
+def compute_stable_bce_cost(Y, Z):
+    """
+    This function computes the "Stable" Binary Cross-Entropy(stable_bce) Cost and returns the Cost and its
+    derivative w.r.t Z_last(the last linear node) .
+    The Stable Binary Cross-Entropy Cost is defined as:
+    => (1/m) * np.sum(max(Z,0) - ZY + log(1+exp(-|Z|)))
+    Args:
+        Y: labels of data
+        Z: Values from the last linear node
+    Returns:
+        cost: The "Stable" Binary Cross-Entropy Cost result
+        dZ_last: gradient of Cost w.r.t Z_last
+    """
+    m = Y.shape[1]
+
+    cost = (1/m) * np.sum(np.maximum(Z, 0) - Z*Y + np.log(1+ np.exp(- np.abs(Z))))
+    dZ_last = (1/m) * ((1/(1+np.exp(- Z))) - Y)  # from Z computes the Sigmoid so P_hat - Y, where P_hat = sigma(Z)
+
+    return cost, dZ_last
+
+def compute_keras_like_bce_cost(Y, P_hat, from_logits=False):
+    """
+    This function computes the Binary Cross-Entropy(stable_bce) Cost function the way Keras
+    implements it. Accepting either probabilities(P_hat) from the sigmoid neuron or values direct
+    from the linear node(Z)
+    Args:
+        Y: labels of data
+        P_hat: Probabilities from sigmoid function
+        from_logits: flag to check if logits are being provided or not(Default: False)
+    Returns:
+        cost: The "Stable" Binary Cross-Entropy Cost result
+        dZ_last: gradient of Cost w.r.t Z_last
+    """
+    if from_logits:
+        # assume that P_hat contains logits and not probabilities
+        return compute_stable_bce_cost(Y, Z=P_hat)
+
+    else:
+        # Assume P_hat contains probabilities. So make logits out of them
+
+        # First clip probabilities to stable range
+        EPSILON = 1e-07
+        P_MAX = 1- EPSILON  # 0.9999999
+
+        P_hat = np.clip(P_hat, a_min=EPSILON, a_max=P_MAX)
+
+        # Second, Convert stable probabilities to logits(Z)
+        Z = np.log(P_hat/(1-P_hat))
+
+        # now call compute_stable_bce_cost
+        return compute_stable_bce_cost(Y, Z)
